4afd7df5dfbbd8cd03923be331501c6117d5bbad
cskksc <ckoparkar@live.in>
2015-05-29 17:32:08 +0530

    Get names of all classes in the app.

--- lib/ext/viz_doc.rb
+++ lib/ext/viz_doc.rb
@@ -11,9 +11,28 @@ module VizDoc
     attr_accessor :logger, :bindings_map, :debug
   end
 
+  # folders = ["app/services", "app/controllers", "app/models"]
+  
+  files = Dir["app/**/*.rb"]
+  files = files.map {|f| f.gsub(/app\/[a-zA-Z]*\//,"")}
+  files = files.map {|f| f.gsub(/\.rb/,"")}
+
+  classes = files.reduce([]) do |acc, f|
+    parts = f.split('/')
+    if parts.size > 1
+      ## inside a module. join with ::
+      x = parts.map do |part|
+        part.split('_').map{|o| o.capitalize}.join
+      end
+      acc << x.join('::')
+    else
+      acc << parts.first.split('_').map{|o| o.capitalize}.join
+    end
+  end
+
   set_trace_func proc { |event, file, line, id, binding, classname|
     begin
-      if ['call', 'return'].include?(event) and ['Lawn'].include?(classname.name.split("::").first)
+      if ['call', 'return'].include?(event) and classes.include?(classname.name.split("::").first)
 
 
         #if VizDoc.bindings_map[classname.to_s].nil?
